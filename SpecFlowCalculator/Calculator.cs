e88f127c2eb489bd9f2865aa4323dda6f2636a0b
Sheila Coles <sheila.coles@ukho.gov.uk>
2022-10-21 15:00:45 +0100

    calculator class

--- SpecFlowCalculator/Calculator.cs
+++ SpecFlowCalculator/Calculator.cs
@@ -2,6 +2,12 @@
 {
     public class Calculator
     {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
 
+        public int Add()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
